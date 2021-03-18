83ea2bb2873c956dc4985fffa42cf41b7afe4cc0
danielle076 <d.vandenakker@novi-education.nl>
2021-03-18 09:38:43 +0100

    added good morning, afternoon and night

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Uren testen: const date = new Date(2021, 1, 1, 20);
+const date = new Date();
+const currentTime = date.getHours();
+
+// Geeft de uren op dit moment
+console.log(currentTime);
+
+// Text voor de verschillende dagdelen
+let greeting;
+
+// Kleur voor de verschillende dagdelen
+const customStyle = {
+    color: ""
+};
+
+// Statement voor de verschillende dagdelen
+if (currentTime < 12) {
+    greeting = "Good Morning";
+    customStyle.color = "red";
+} else if (currentTime < 18) {
+    greeting = "Good Afternoon";
+    customStyle.color = "green";
+} else {
+    greeting = "Good Night";
+    customStyle.color = "blue";
+}
+
+// Aanmaken van de h1
+// Text "greeting" toevoegen aan h1
+// Kleur "customStyle" toevoegen aan h1
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <h1 className="heading" style={customStyle}>
+        {greeting}
+    </h1>,
+    document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
